f29047635348e358d16e078a747dadd5130afa2a
Sinan <sinan.sarykaya@gmail.com>
2022-08-20 20:40:25 +0200

    Theme changed

--- js/app.js
+++ js/app.js
@@ -2,8 +2,10 @@ const guessInput = document.querySelector(".guessInput");
 const guessButton = document.querySelector(".guessButton");
 const lifeDiv = document.querySelector(".life");
 const resetButton = document.querySelector(".resetButton");
-const scoreList = document.querySelector(".scoreList");
+const scoreList = document.querySelector(".scores");
 const bestScore = document.querySelector(".bestScore");
+const arrowDown = document.querySelector(".down");
+const arrowUp = document.querySelector(".up");
 
 let getScore = JSON.parse(localStorage.getItem("playerScore"));
 
@@ -13,33 +15,47 @@ let isEnd = false;
 
 console.log(randomNumber);
 
+const getPoint = () => {
+  getScore
+    .reverse()
+    .slice(0, 5)
+    .map((score, i) => {
+      scoreList.innerHTML += `<div class="score-box" key="${i}">${
+        i + 1
+      }. ${score}</div>`;
+    });
+};
+
 document.addEventListener("keyup", (e) => {
   if (e.key == "Enter") {
     checkResult();
   }
 });
-console.log(getScore);
 const checkResult = () => {
   if (life > 0 && !isEnd) {
     if (Number(guessInput.value) === randomNumber) {
       endGame();
     } else if (Number(guessInput.value) < randomNumber) {
       life -= 1;
-      lifeDiv.innerHTML = `❤️ ${life} Yukari`;
+      lifeDiv.innerHTML = `<i class="fa-solid fa-heart"></i> ${life}`;
+      arrowUp.style.opacity = "1";
+      arrowDown.style.opacity = "0.5";
       guessInput.value = "";
     } else {
       life -= 1;
       console.log("Assagi");
-      lifeDiv.innerHTML = `❤️ ${life} Assagi`;
+      lifeDiv.innerHTML = `<i class="fa-solid fa-heart"></i> ${life}`;
+      arrowUp.style.opacity = "0.5";
+      arrowDown.style.opacity = "1";
       guessInput.value = "";
     }
   } else {
     endGame();
   }
 };
-if (getScore) {
-  bestScore.innerHTML = `Best Score ${Math.max(...getScore)}`;
-}
+// if (getScore) {
+//   bestScore.innerHTML = `Best Score ${Math.max(...getScore)}`;
+// }
 
 function endGame() {
   isEnd = true;
@@ -49,34 +65,26 @@ function endGame() {
   ) {
     lifeDiv.innerHTML = `❤️ ${life} Kazandin`;
     setPoint();
+    if (getScore) {
+      getPoint();
+    }
   } else if (life === 0 && Number(guessInput.value) !== randomNumber) {
     lifeDiv.innerHTML = `❤️ ${life} Kaybettin`;
   }
+}
 
+if (getScore) {
   getPoint();
 }
 
-const getPoint = () => {
-  getScore
-    .reverse()
-    .slice(0, 10)
-    .map((score, i) => {
-      scoreList.innerHTML += `<li key="${i}">${score}</li>`;
-    });
-};
-
-getPoint();
-
 const setPoint = () => {
   let score = [];
-
   if (getScore) {
     getScore.push(life);
     score = getScore;
   } else {
     score[0] = life;
   }
-
   localStorage.setItem("playerScore", JSON.stringify(score));
 };
 
@@ -84,8 +92,9 @@ resetButton.addEventListener("click", () => {
   randomNumber = Math.round(Math.random() * 100);
   life = 5;
   isEnd = false;
-  lifeDiv.innerHTML = `❤️ ${life}`;
+  lifeDiv.innerHTML = `<i class="fa-solid fa-heart"></i> ${life}`;
   guessInput.value = "";
+  arrowUp.style.opacity = "0.5";
+  arrowDown.style.opacity = "0.5";
   console.log(randomNumber);
 });
-console.log(getScore.length);
